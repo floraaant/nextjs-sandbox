55adea9ab8cfcea142fd96d7bdb7f956b63e88c9
Florant Hauguel <floraaant@macbook-pro-de-florant.home>
2024-01-17 21:49:10 +0100

    test 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,33 @@ const config: Config = {
       gridTemplateColumns: {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
       colors: {
         blue: {
           400: '#2589FE',
           500: '#0070F3',
           600: '#2F6FEB',
         },
+        tahiti: {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+        },
+      },
+      fontFamily: {
+        sans: ['Open sans', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
       },
     },
     keyframes: {
